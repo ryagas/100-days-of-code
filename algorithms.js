b23223ee4c0a6b51b343ba3f89463cd33221af1e
Ryan Campbell <me@ryancampbell.name>
2017-01-11 21:44:34 -0800

    Palindrome function implemented!

--- algorithms.js
+++ algorithms.js
@@ -1,54 +1,54 @@
 
 
 function reverseString(str) {
-    return str.split("").reverse().join("");
+  return str.split("").reverse().join("");
 }
 
 reverseString("hello");
 
 function factorialize(num) {
-    var result = 1;
-    for (i=num; i>0; i--) {
-        result *= i;
-    }
+  var result = 1;
+  for (i = num; i > 0; i--) {
+    result *= i;
+  }
 
-    return result;
+  return result;
 }
 
 
 function palindrome(str) {
   // Good luck!
 
-  var regEx = '(?! [a-zA-Z0-9])';
+  var aStr = str.toLowerCase().replace(/[^a-zA-Z0-9]+/g, '');
+  var halfway = 0;
 
-  // Step 1: remove non-alphanumeric characters 
-  var originalStr = str.replace(regEx, '');
-  console.log (originalStr);
-  
-  // Step 2: lowercase all
-  originalStr.toLowerCase();
-
-  // Step 3: turn the string into an array
-  var origArray = originalStr.split('');
-
-  // Step 4: copy to temp array
-  var reverseArray = origArray;
-
-  // Step 5: reverse array
-  reverseArray.reverse();
-
-  // Step 6: compare, returning the result
-  for (var index = 0; index < reverseArray.length; index++) {
-      result = true;
-      if (reverseArray[index] != origArray[index]) {
-          result = false;
-          return result;
-      }
+  // empty string?
+  if (aStr === "") {
+    return false;
   }
 
-  return result;
+  // even or odd length? set the pivot
+  if ((aStr.length) % 2 === 0) {
+    halfway = (aStr.length) / 2;
+  }
+  else {
+    // length = 1 => palindrome
+    if (aStr.length === 1) {
+      return true;
+    }
+    else {
+      // odd length? ignore middle
+      halfway = (aStr.length - 1) / 2;
+    }
+  }
 
+  // compare first with last then the next
+  for (var x = 0; x < halfway; x++) {
+    // compare chars and if they don't match, we're done
+    if (aStr[x] != aStr.slice(-1 - x)[0]) {
+      return false;
+    }
+  }
+  // if we're here, we have a palindrome!
+  return true;
 }
-
-
-palindrome("eye");
